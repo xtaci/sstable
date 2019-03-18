3b19302c5776c1fa0c6717dfa1758de10d557a1f
xtaci <daniel820313@gmail.com>
2019-03-18 12:17:22 +0800

    init

---
+++ main.go
@@ -0,0 +1,436 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"container/heap"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// pre-processing stage
+// an in-memory sorter
+
+// key-value construction
+// rawEntry binary format:
+// bytesSize int32
+// bytesPtr int32
+type rawEntry []byte
+
+const entrySize = 8
+
+func (e rawEntry) sz() int32  { return int32(binary.LittleEndian.Uint32(e[:])) }
+func (e rawEntry) ptr() int32 { return int32(binary.LittleEndian.Uint32(e[4:])) }
+
+// value binds to specific bytes buffer
+func (e rawEntry) value(buf []byte) rawValue { return buf[e.ptr():][:e.sz()] }
+
+// value binary format
+// ord int64
+// line []byte
+type rawValue []byte
+
+func (v rawValue) ord() int64   { return int64(binary.LittleEndian.Uint64(v[:])) }
+func (v rawValue) line() []byte { return v[8:] }
+
+type entry struct {
+	str []byte
+	ord int64
+}
+
+// split large slice set into a group of small sets
+// for limiting memory usage, we write content backwords
+// and write idx forwards, as:
+// [key0,key1,...keyN,..... valueN, .... value0]
+type dataSet struct {
+	buf         []byte
+	idxWritten  int
+	dataWritten int
+	dataPtr     int // point to last non-writable place, dataPtr -1 will be writable
+
+	idxPtr  int // point to next index place
+	idxSize int
+	swapbuf []byte
+}
+
+func newDataSet(sz int) *dataSet {
+	e := new(dataSet)
+	e.buf = make([]byte, sz)
+	e.dataPtr = sz
+	e.swapbuf = make([]byte, entrySize)
+	return e
+}
+
+func (s *dataSet) Add(line []byte, ord int64) bool {
+	sz := len(line) + 8
+	if s.idxWritten+s.dataWritten+sz+entrySize >= len(s.buf) {
+		return false
+	}
+
+	// write data
+	s.dataPtr -= sz
+	s.dataWritten += sz
+	binary.LittleEndian.PutUint64(s.buf[s.dataPtr:], uint64(ord))
+	copy(s.buf[s.dataPtr+8:], line)
+
+	// write idx
+	binary.LittleEndian.PutUint32(s.buf[s.idxPtr:], uint32(sz))
+	binary.LittleEndian.PutUint32(s.buf[s.idxPtr+4:], uint32(s.dataPtr))
+	s.idxPtr += entrySize
+	s.idxWritten += entrySize
+
+	return true
+}
+
+// return the ith entry in binary form
+func (s *dataSet) e(i int) rawEntry {
+	return rawEntry(s.buf[i*entrySize : i*entrySize+entrySize])
+}
+
+// return the ith element in text form
+func (s *dataSet) get(i int) entry {
+	v := s.e(i).value(s.buf)
+	return entry{v.line(), v.ord()}
+}
+
+func (s *dataSet) Len() int { return s.idxWritten / entrySize }
+func (s *dataSet) Less(i, j int) bool {
+	v1 := s.e(i).value(s.buf)
+	v2 := s.e(j).value(s.buf)
+	return bytes.Compare(v1.line(), v2.line()) < 0
+}
+
+func (s *dataSet) Swap(i, j int) {
+	copy(s.swapbuf, s.e(i))
+	copy(s.e(i), s.e(j))
+	copy(s.e(j), s.swapbuf)
+}
+
+// data set reader
+type dataSetReader struct {
+	set  *dataSet
+	head int
+	elem entry
+}
+
+func newDataSetReader(set *dataSet) *dataSetReader {
+	if set.Len() == 0 {
+		return nil
+	}
+	esr := new(dataSetReader)
+	esr.set = set
+	esr.elem = set.get(0)
+	return esr
+}
+
+func (esr *dataSetReader) next() bool {
+	esr.head++
+	if esr.head >= esr.set.Len() {
+		return false
+	}
+	esr.elem = esr.set.get(esr.head)
+	return true
+}
+
+// memory based aggregator
+type memSortAggregator struct {
+	sets []*dataSetReader
+}
+
+func (h *memSortAggregator) Len() int { return len(h.sets) }
+func (h *memSortAggregator) Less(i, j int) bool {
+	return bytes.Compare(h.sets[i].elem.str, h.sets[j].elem.str) < 0
+}
+func (h *memSortAggregator) Swap(i, j int)      { h.sets[i], h.sets[j] = h.sets[j], h.sets[i] }
+func (h *memSortAggregator) Push(x interface{}) { h.sets = append(h.sets, x.(*dataSetReader)) }
+func (h *memSortAggregator) Pop() interface{} {
+	n := len(h.sets)
+	x := h.sets[n-1]
+	h.sets = h.sets[0 : n-1]
+	return x
+}
+
+// words sorter for big memory
+type sortWords struct {
+	sets    []*dataSet
+	setSize int
+	limit   int // max total memory usage
+}
+
+func (h *sortWords) Len() int {
+	n := 0
+	for k := range h.sets {
+		n += h.sets[k].Len()
+	}
+	return n
+}
+
+func (h *sortWords) Serialize(w io.Writer) {
+	if len(h.sets) > 0 {
+		agg := new(memSortAggregator)
+		for k := range h.sets {
+			log.Println("sorting sets#", k)
+			sort.Sort(h.sets[k])
+			heap.Push(agg, newDataSetReader(h.sets[k]))
+		}
+		log.Println("merging sorted sets to file")
+
+		written := 0
+		esr := heap.Pop(agg).(*dataSetReader)
+		last := esr.elem
+		last_cnt := 1
+		if esr.next() {
+			heap.Push(agg, esr)
+		}
+
+		for agg.Len() > 0 {
+			esr = heap.Pop(agg).(*dataSetReader)
+			elem := esr.elem
+			if bytes.Compare(elem.str, last.str) == 0 { // condense output
+				last_cnt++
+			} else {
+				fmt.Fprintf(w, "%v,%v,%v\n", string(last.str), last.ord, last_cnt)
+				last = elem
+				last_cnt = 1
+				written++
+			}
+			if esr.next() {
+				heap.Push(agg, esr)
+			}
+		}
+		fmt.Fprintf(w, "%v,%v,%v\n", string(last.str), last.ord, last_cnt)
+		written++
+		log.Println("written", written, "elements")
+		h.sets = nil
+	}
+}
+
+// Add controls the memory for every input
+func (h *sortWords) Add(line []byte, ord int64) bool {
+	if h.sets == nil { // init first one
+		h.sets = []*dataSet{newDataSet(h.setSize)}
+	}
+
+	set := h.sets[len(h.sets)-1]
+	if !set.Add(line, ord) {
+		if h.setSize*(len(h.sets)+1) > h.limit { // limit reached
+			return false
+		}
+		newSet := newDataSet(h.setSize)
+		h.sets = append(h.sets, newSet)
+		newSet.Add(line, ord)
+	}
+	return true
+}
+
+func (h *sortWords) init(limit int) {
+	h.setSize = 1 << 24 // 16MB set
+	h.limit = limit
+	if h.limit < h.setSize {
+		h.limit = h.setSize
+	}
+}
+
+// sort2Disk writes strings with it's ordinal and count
+// xxxxx,1234,1
+// aaaa,5678,10
+func sort2Disk(r io.Reader, memLimit int) int {
+	h := new(sortWords)
+	h.init(memLimit)
+	var ord int64
+	parts := 0
+
+	// file based serialization
+	fileDump := func(hp *sortWords, path string) {
+		f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hp.Serialize(f)
+		runtime.GC()
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		if !h.Add(scanner.Bytes(), ord) {
+			fileDump(h, fmt.Sprintf("part%v.dat", parts))
+			log.Println("chunk#", parts, "written")
+			parts++
+			h.Add(scanner.Bytes(), ord)
+		}
+		ord++
+
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal("error reading from source")
+	}
+
+	if h.Len() > 0 {
+		fileDump(h, fmt.Sprintf("part%v.dat", parts))
+		log.Println("chunk#", parts, "written")
+		parts++
+	}
+	return parts
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// disk streaming stage
+type streamReader struct {
+	scanner *bufio.Scanner
+	str     string // the head element
+	ord     string
+	cnt     string
+}
+
+func (sr *streamReader) next() bool {
+	if sr.scanner.Scan() {
+		strs := strings.Split(sr.scanner.Text(), ",")
+		sr.str = strs[0]
+		sr.ord = strs[1]
+		sr.cnt = strs[2]
+		return true
+	}
+	return false
+}
+
+func newStreamReader(r io.Reader) *streamReader {
+	sr := new(streamReader)
+	sr.scanner = bufio.NewScanner(r)
+	sr.scanner.Split(bufio.ScanLines)
+	return sr
+}
+
+// streamAggregator always pop the min string
+type streamAggregator struct {
+	entries []*streamReader
+}
+
+func (h *streamAggregator) Len() int           { return len(h.entries) }
+func (h *streamAggregator) Less(i, j int) bool { return h.entries[i].str < h.entries[j].str }
+func (h *streamAggregator) Swap(i, j int)      { h.entries[i], h.entries[j] = h.entries[j], h.entries[i] }
+func (h *streamAggregator) Push(x interface{}) { h.entries = append(h.entries, x.(*streamReader)) }
+func (h *streamAggregator) Pop() interface{} {
+	n := len(h.entries)
+	x := h.entries[n-1]
+	h.entries = h.entries[0 : n-1]
+	return x
+}
+
+type countedEntry struct {
+	str string
+	ord int64
+	cnt int64
+}
+
+func merger(parts int) chan countedEntry {
+	ch := make(chan countedEntry, 4096)
+	go func() {
+		files := make([]*os.File, parts)
+		h := new(streamAggregator)
+		for i := 0; i < parts; i++ {
+			f, err := os.Open(fmt.Sprintf("part%v.dat", i))
+			if err != nil {
+				log.Fatal(err)
+			}
+			files[i] = f
+			sr := newStreamReader(f)
+			sr.next() // fetch first string,ord
+			heap.Push(h, sr)
+		}
+
+		for h.Len() > 0 {
+			sr := heap.Pop(h).(*streamReader)
+			ord, _ := strconv.ParseInt(sr.ord, 10, 64)
+			cnt, _ := strconv.ParseInt(sr.cnt, 10, 64)
+			ch <- countedEntry{sr.str, ord, cnt}
+			if sr.next() {
+				heap.Push(h, sr)
+			}
+		}
+		close(ch)
+
+		for _, f := range files[:] {
+			if err := f.Close(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	return ch
+}
+
+// findUnique reads from r with a specified bufsize
+// and trys to find the first unique string in this file
+func findUnique(r io.Reader, memLimit int) {
+	// step.1 sort into file chunks
+	parts := sort2Disk(r, memLimit)
+	log.Println("generated", parts, "parts")
+	// step2. sequential output of all parts
+	ch := merger(parts)
+	log.Println("beginning merged sequential output")
+
+	// step3. loop through the sorted string chan
+	// and find the unique string with lowest ord
+	var target_str string
+	var target_ord int64
+	var hasSet bool
+
+	var last_str string
+	var last_ord int64
+	var last_cnt int64
+	if e, ok := <-ch; ok {
+		last_str = e.str
+		last_ord = e.ord
+		last_cnt = e.cnt
+	} else {
+		log.Println("empty set")
+		return
+	}
+
+	compareTarget := func() {
+		if last_cnt == 1 {
+			// found new unique string, compare with the ordinal
+			if !hasSet {
+				target_str = last_str
+				target_ord = last_ord
+				hasSet = true
+			} else if last_ord < target_ord {
+				target_str = last_str
+				target_ord = last_ord
+			}
+		}
+	}
+
+	// read through the sorted string chan
+	for e := range ch {
+		if last_str == e.str {
+			last_cnt += e.cnt
+		} else {
+			compareTarget()
+			last_str = e.str
+			last_ord = e.ord
+			last_cnt = e.cnt
+		}
+	}
+
+	// make sure the final words is considered
+	compareTarget()
+
+	if hasSet {
+		log.Println("Found the first unique string:", string(target_str), "appears at:", target_ord)
+	} else {
+		log.Println("Unique string not found!")
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"testing"
+	"time"
+)
+
+const testfile = "10G.data"
+const Mega = 1024 * 1024
+
+func init() {
+	go http.ListenAndServe(":6060", nil)
+	f, err := os.Open(testfile)
+	if err != nil {
+		log.Println("generating", testfile)
+		generate10G()
+	} else {
+		f.Close()
+	}
+}
+
+type dummyReader struct {
+	count int
+	max   int
+	rnd   *rand.Rand
+}
+
+func (dr *dummyReader) Read(p []byte) (n int, err error) {
+	var alpha = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ             ")
+	if dr.count == dr.max {
+		return 0, io.EOF
+	}
+
+	remain := len(p)
+	idx := 0
+	for remain > 0 {
+		p[idx] = alpha[dr.rnd.Intn(len(alpha))]
+		idx++
+		remain--
+		dr.count++
+		if dr.count == dr.max {
+			return idx, io.EOF
+		}
+	}
+
+	return idx, nil
+}
+
+func newDummyReader(cap int) *dummyReader {
+	dr := new(dummyReader)
+	dr.max = cap
+	dr.rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+	return dr
+}
+
+func generate10G() {
+	dr := newDummyReader(10 * 1024 * Mega)
+	f, err := os.OpenFile(testfile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.Copy(f, dr)
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestFindUniqueString(t *testing.T) {
+	t0 := bytes.NewBufferString("   ")
+	findUnique(t0, 128*Mega)
+	t1 := bytes.NewBufferString("a a b b c b") // 4
+	findUnique(t1, 128*Mega)
+	t2 := bytes.NewBufferString("a a a a a a") // no
+	findUnique(t2, 128*Mega)
+	t3 := bytes.NewBufferString("a b c d e a") //1
+	findUnique(t3, 128*Mega)
+	t4 := bytes.NewBufferString("a a a a a b") // 5
+	findUnique(t4, 128*Mega)
+}
+
+func TestFindUnique100M(t *testing.T) {
+	file, err := os.Open(testfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	findUnique(io.LimitReader(file, 100*Mega), 10*Mega)
+}
+func TestFindUnique1G(t *testing.T) {
+	file, err := os.Open(testfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	findUnique(io.LimitReader(file, 1000*Mega), 100*Mega)
+}
+
+func TestFindUnique10G(t *testing.T) {
+	file, err := os.Open(testfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	findUnique(io.LimitReader(file, 10000*Mega), 1000*Mega)
+}
